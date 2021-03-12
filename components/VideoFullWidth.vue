12dfec7bf16fe9ef67ddf9b2dbce2c4a542451d6
Meyer <jack.meyer@emerson.com>
2021-03-12 13:16:50 -0500

    adding video section

---
+++ components/VideoFullWidth.vue
@@ -0,0 +1,32 @@
+<template>
+<div  v-editable="blok" class="bg-white">
+<video width="100%" height="240px" autoplay muted>
+  <source :src="blok.video.filename" type="video/mp4">
+</video>
+</div>
+</template>
+<script>
+export default {
+  props: {
+    blok: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+<style scoped>
+div {
+  line-height: 0;
+}
+video {
+    position: absolute;
+    border: none;
+    object-fit: cover;
+    outline: none;
+    border: none;
+    box-shadow: none;
+    overflow: hidden;
+    max-height: 350px;
+}
+</style>
--- plugins/components.js
+++ plugins/components.js
@@ -3,10 +3,11 @@ import Page from '~/components/Page.vue'
 import Hero from '~/components/Hero.vue'
 import Grid from '~/components/Grid.vue'
 import Feature from '~/components/Feature.vue'
-
+import VideoFullWidth from '~/components/VideoFullWidth.vue'
 
 Vue.component('page', Page)
 Vue.component('hero', Hero)
 Vue.component('grid', Grid)
 Vue.component('feature', Feature)
+Vue.component('video-full-width', VideoFullWidth)
 
