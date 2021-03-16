4ca3dbaf3da8d9ea6546ec26b1b3097ac849e235
Meyer <jack.meyer@emerson.com>
2021-03-16 15:44:23 -0400

    adding missing name attributes to form

--- components/ContactForm.vue
+++ components/ContactForm.vue
@@ -94,11 +94,12 @@
           ></textarea>
         </div>
       </div>
-       <button
-          name="send" class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-yellow-600 hover:bg-yellow-500 md:py-4 md:text-lg md:px-10"
-        >
-          SEND
-        </button>
+      <button
+        type="submit"
+        class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-yellow-600 hover:bg-yellow-500 md:py-4 md:text-lg md:px-10"
+      >
+        SEND
+      </button>
     </div>
   </form>
 </template>
