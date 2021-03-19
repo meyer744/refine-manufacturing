a2fa1dd7f32131171fa9f007c8a59728ffdde9ae
Meyer <jack.meyer@emerson.com>
2021-03-19 14:56:59 -0400

    adjusting page header

--- components/PageHeader.vue
+++ components/PageHeader.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="header-image" v-editable="blok" :style="{ backgroundImage: `url(${blok.image.filename})`, backgroundColor: blok.backgroundColor.color,   minHeight: blok.pixelHeight + 'px'}">
   <div class="header-title flex flex-col space-y-8 text-center">
-    <h1 class="text-4xl md:text-5xl lg:text-5xl xl:text-6xl 2xl:text-6xl font-bold text-white">{{blok.title}}</h1>
+    <h1 class="text-4xl md:text-5xl lg:text-5xl xl:text-5xl 2xl:text-6xl font-bold text-white">{{blok.title}}</h1>
   </div>
 </div>
 </template>
@@ -27,7 +27,7 @@ export default {
 
 .header-title {
   position: absolute;
-  top: 65%;
+  top: 62%;
   left: 50%;
   transform: translate(-50%, -50%);
   color: white;
